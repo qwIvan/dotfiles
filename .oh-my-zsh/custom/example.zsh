29d7aeb21f9ec4c34b18f0d05182a79059de94e3
ivan <qw7692336@gmail.com>
2016-04-23 15:11:05 +0800

    update

--- .oh-my-zsh/custom/example.zsh
+++ .oh-my-zsh/custom/example.zsh
@@ -6,7 +6,6 @@
 #
 #alias lu="ls -u"
 i() curl ip.cn/$1
-alias xx=extract
 alias tree="tree -C"
 transfer() {
     # write to output to tmpfile because of progress bar
@@ -17,7 +16,6 @@ transfer() {
 }
 alias tpp="tree --prune -P"
 alias c=ccat
-alias free="free -h"
 export CHEATCOLORS=true
 alias rsync-compare="rsync -ani -vv"
 rsync-compare-into() rsync -aHxi -vv --progress --compare-dest=$2 $1 $3
@@ -26,15 +24,15 @@ alias t2="tree -L 2"
 alias t3="tree -L 3"
 alias t4="tree -L 4"
 alias t5="tree -L 5"
-alias -g a1="| awk \"{print \\\$1}\""
-alias -g a2="| awk \"{print \\\$2}\""
-alias -g a3="| awk \"{print \\\$3}\""
-alias -g a4="| awk \"{print \\\$4}\""
-alias -g a5="| awk \"{print \\\$5}\""
+alias -g A1="| awk \"{print \\\$1}\""
+alias -g A2="| awk \"{print \\\$2}\""
+alias -g A3="| awk \"{print \\\$3}\""
+alias -g A4="| awk \"{print \\\$4}\""
+alias -g A5="| awk \"{print \\\$5}\""
 alias -g L="| less -R"
-alias -g E=" --help"
+alias -g E="--help"
 alias -g X="| xargs"
-alias -g Xi="| xargs -i"
+alias -g XI="| xargs -i"
 alias x=extract
 alias -g PP="\`clippaste\`"
 unalias ping
